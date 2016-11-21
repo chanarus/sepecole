09f01da90d155580bc6652943e1ded627f7e84d6
HirushiVidyaratna <emorushwofzie@gmail.com>
2016-11-21 20:26:23 +0530

    modify enter marks

--- application/controllers/marks.php
+++ application/controllers/marks.php
@@ -13,6 +13,7 @@ class marks extends CI_Controller
     {
         parent::__construct();
         $this->load->model('marks_model');
+        $this->load->helper('date');
 
         if (!$this->session->userdata('logged_in')) {
             redirect('login');
@@ -20,40 +21,69 @@ class marks extends CI_Controller
     }
     
   /**
-   * load add marks page
+   * Load enter marks page
    */
   function index()
   {
       $data['page_title'] = " Student Grading Management";
       $data['user_type'] = $this->session->userdata['user_type'];
       $data['navbar'] = "admin";
+      $data['ids'] = $this->marks_model->get_all_grades();
+      //var_dump($data['ids']);
 
       $this->load->view('templates/header', $data);
       $this->load->view('navbar_main', $data);
       $this->load->view('navbar_sub', $data);
       $this->load->view('Marks/exam_details', $data);
       $this->load->view('templates/footer');
+
   }
-
-
   /**
-  *
+  * enter exam marks
   */
-  function exam_marks() {
-    $data['page_title'] = " Student Grading Management";
-    $data['user_type'] = $this->session->userdata['user_type'];
-    $data['navbar'] = "admin";
+  function exam_marks(){
+      $data['page_title'] = " Student Grading Management";
+      $data['user_type'] = $this->session->userdata['user_type'];
+      $data['navbar'] = "admin";
+      $data['ids'] = $this->marks_model->get_all_grades();
+
+     $this->load->library('form_validation');
 
 
-    $this->load->view('templates/header', $data);
-    $this->load->view('navbar_main', $data);
-    $this->load->view('navbar_sub', $data);
-    $this->load->view('Marks/exam_marks', $data);
-    $this->load->view('templates/footer');
+
+      $this->form_validation->set_rules('examname','examname','required|min_length[6]');
+      $this->form_validation->set_rules('year','year','required');
+      $this->form_validation->set_rules('grade', 'grade', 'required|callback_grade_selected');
+      $this->form_validation->set_error_delimiters('<br /><span class="error">', '</span>');
+
+      if ($this->form_validation->run() == FALSE) {
+          $this->load->view('templates/header', $data);
+          $this->load->view('navbar_main', $data);
+          $this->load->view('navbar_sub', $data);
+          $this->load->view('Marks/exam_details', $data);
+          $this->load->view('templates/footer');
+      }
+      else{
+          $examname = $this->input->post('examname');
+          $grade = $this->input->post('grade');
+          $year = $this->input->post('year');
+          $start_date = $this->input->post('start_date');
+          $end_date = $this->input->post('end_date');
+
+          $this->marks_model->enter_marks1($examname,$year,$grade,$start_date,$end_date);
+//
+          $data['succ_message'] = "Succesfully inserted";
+          $this->load->view('templates/header', $data);
+          $this->load->view('navbar_main', $data);
+          $this->load->view('navbar_sub', $data);
+          $this->load->view('Marks/exam_details', $data);
+          $this->load->view('templates/footer');
+      }
+
+       //$data['exams'] = $this->marks_model->enter_marks();
   }
-
   /**
-  *
+  * marks lists can be viewed.
   */
   function view_marks() {
     $data['page_title'] = " Student Grading Management";
@@ -67,8 +97,19 @@ class marks extends CI_Controller
     $this->load->view('templates/footer');
   }
 
+    function mark_marks() {
+        $data['page_title'] = " Student Grading Management";
+        $data['user_type'] = $this->session->userdata['user_type'];
+        $data['navbar'] = "admin";
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('navbar_main', $data);
+        $this->load->view('navbar_sub', $data);
+        $this->load->view('Marks/exam_marks', $data);
+        $this->load->view('templates/footer');
+    }
   /**
-  *
+  * Generate reports of marks
   */
   function genarate_reports() {
     $data['page_title'] = " Student Grading Management";
@@ -80,5 +121,20 @@ class marks extends CI_Controller
     $this->load->view('navbar_sub', $data);
     $this->load->view('Marks/genarate_reports', $data);
     $this->load->view('templates/footer');
+
+
+
+
+
   }
+    function grade_selected() {
+        $grade = $this->input->post('grade');
+        if ($grade == 'select_grade') {
+            $this->form_validation->set_message('grade_selected', 'Please select a grade');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
 }
--- application/views/Marks/exam_marks.php
+++ application/views/Marks/exam_marks.php
@@ -20,7 +20,7 @@
                       </div>
                       <div class="media-body">
                           <h4 class="media-heading">Exam Details</h4>
-                          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                          The exam details is displayed.
                       </div>
                   </div>
               </div><hr>
